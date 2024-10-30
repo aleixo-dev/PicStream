74cc1a089319cab3275028c9c9f0857df7884927
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2024-10-30 11:12:04 -0300

    feat: add notification download description

--- app/src/main/java/com/nicolas/picstream/downloader/Downloader.kt
+++ app/src/main/java/com/nicolas/picstream/downloader/Downloader.kt
@@ -1,6 +1,6 @@
 package com.nicolas.picstream.downloader
 
 interface Downloader {
-    fun downloadFile (url : String, title : String) : Long
+    fun downloadFile (url : String, title : String, description : String) : Long
     fun downloadVideo (url : String, title : String) : Long
 }
