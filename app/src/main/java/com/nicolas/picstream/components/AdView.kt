7434fdcdd2c3146a6493b04eb5bd158a581a4dfd
Nicolas Aleixo <75820713+aleixo-dev@users.noreply.github.com>
2024-10-30 11:17:46 -0300

    [crashlytics] implement crashlytics service (#7)
    * feat: implement crashlytics service
    
    * feat: implement build config ad mob key

--- app/src/main/java/com/nicolas/picstream/components/AdView.kt
+++ app/src/main/java/com/nicolas/picstream/components/AdView.kt
@@ -5,14 +5,18 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.nicolas.picstream.BuildConfig
 import com.nicolas.picstream.R
 
 fun showInterstitialAd(context: Context, onShowAd: (interstitialAd: InterstitialAd) -> Unit) {
 
-    var interstitialAd : InterstitialAd?
+    var interstitialAd: InterstitialAd?
+    val interstitialKey =
+        if (BuildConfig.DEBUG)
+            context.getString(R.string.dummy_admob_interstitial_key) else BuildConfig.INTERSTITIAL_KEY
 
     InterstitialAd.load(context,
-        context.getString(R.string.dummy_admob_interstitial_key),
+        interstitialKey,
         AdRequest.Builder().build(),
         object : InterstitialAdLoadCallback() {
             override fun onAdFailedToLoad(error: LoadAdError) {
